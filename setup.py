4abde4720295e5d7502cafe68211e05a19abee11
Jonathan MacCarthy <jkmacc@lanl.gov>
2019-12-18 15:35:35 -0700

    Fix setup.py, reorg modules.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" 
+"""
 e1: Python support for the e1 compression format.
 
 
 """
-import glob
 from setuptools import setup, Extension
 
 # from numpy.distutils.core import setup, Extension
@@ -23,14 +22,12 @@ setup(name='e1',
       long_description_content_type="text/markdown", # setuptools >= 38.6.0
       author='Jonathan MacCarthy',
       author_email='jkmacc@lanl.gov',
-      packages=['pisces', 'pisces.schema','pisces.io','pisces.tables',
-                'pisces.commands'],
       url='https://github.com/LANL-seismoacoustics/e1',
       download_url='https://github.com/LANL-seismoacoustics/e1/tarball/0.1.0',
       keywords=['seismology', 'geophysics'],
       install_requires=['numpy'],
-      ext_package='e1.lib',
-      ext_modules=[Extension('libe1', ['src/e_compression.c'])],
+      py_modules=['e1'],
+      ext_modules=[Extension('_libe1', ['src/e_compression.c'])],
       license='LANL-MIT',
       platforms=['Mac OS X', 'Linux/Unix'],
 )
